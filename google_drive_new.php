b455355b5e0e52501c9dac194aa5577fc7e62dd2
abhishekgandhi11 <abhishekgandhi63@gmail.com>
2018-09-19 02:39:15 +0530

    new test

--- google_drive_new.php
+++ google_drive_new.php
@@ -24,18 +24,22 @@ else
 	 
      $main_folder=$_SESSION['user_name'];
      echo $main_folder;
-	$fileMetadata = new Google_Service_Drive_DriveFile(array(
-        'name' => $main_folder,
-        'mimeType' => 'application/vnd.google-apps.folder'));
-    $file = $drive->files->create($fileMetadata, array('fields' => 'id'));
-    $folderId = $file->id;
-	
+	// $fileMetadata = new Google_Service_Drive_DriveFile(array(
+    //     'name' => $main_folder,
+    //     'mimeType' => 'application/vnd.google-apps.folder'));
+    // $file = $drive->files->create($fileMetadata, array('fields' => 'id'));
+    // $folderId = $file->id;
+    $links = $_SESSION['links'];
+	$graphNode = $_SESSION['GraphNode'];
 	   $albumname=$_SESSION['Selected_albums'];
-	   
-	    foreach ($albumname as $album)
-		{
-            moveToDrive($album,$folderId,$drive);
-        }
+    print_r($links);
+    echo "<br/><br/><pre>";
+        print_r($graphNode);
+    echo "</pre>";
+	    // foreach ($albumname as $album)
+		// {
+        //     moveToDrive($album,$folderId,$drive);
+        // }
    
    
 }
@@ -50,7 +54,11 @@ else
 //********************	
 function moveToDrive($album_id,$folderId,$drive)
 {
-	$img=array("https://scontent.xx.fbcdn.net/v/t1.0-9/39760439_859661354230615_8995455293735305216_o.jpg?_nc_cat=0&oh=c1fe7ab85e829a26f1472d22138dc019&oe=5BEE1000", "https://scontent.xx.fbcdn.net/v/t1.0-0/p75x225/39799614_859661517563932_7874550640016359424_n.jpg?_nc_cat=0&oh=5a886fffc5164bf264fdc26a472d5aea&oe=5C33F5D5", "https://scontent.xx.fbcdn.net/v/t1.0-9/39917099_861599617370122_1588937861418188800_o.jpg?_nc_cat=0&oh=b8c60db2df8bb2278e709fef960e8ac6&oe=5C266E8B","https://scontent.xx.fbcdn.net/v/t1.0-0/p75x225/39944419_859661610897256_1205816779931123712_n.jpg?_nc_cat=0&oh=b828a9f10e9bd7ccc436af0a38afb1bd&oe=5C2CF670");
+    
+    
+    
+    
+    $img=array("https://scontent.xx.fbcdn.net/v/t1.0-9/39760439_859661354230615_8995455293735305216_o.jpg?_nc_cat=0&oh=c1fe7ab85e829a26f1472d22138dc019&oe=5BEE1000", "https://scontent.xx.fbcdn.net/v/t1.0-0/p75x225/39799614_859661517563932_7874550640016359424_n.jpg?_nc_cat=0&oh=5a886fffc5164bf264fdc26a472d5aea&oe=5C33F5D5", "https://scontent.xx.fbcdn.net/v/t1.0-9/39917099_861599617370122_1588937861418188800_o.jpg?_nc_cat=0&oh=b8c60db2df8bb2278e709fef960e8ac6&oe=5C266E8B","https://scontent.xx.fbcdn.net/v/t1.0-0/p75x225/39944419_859661610897256_1205816779931123712_n.jpg?_nc_cat=0&oh=b828a9f10e9bd7ccc436af0a38afb1bd&oe=5C2CF670");
 	
     $fileMetadata1 = new Google_Service_Drive_DriveFile(array(
         'name' => $album_id,
--- test.php
+++ test.php
@@ -276,54 +276,56 @@
 											if(file_exists($user_name)){
 												unlink($user_name);
 											}
-											// function getData($url)
-											// {
-											// 	//  Initiate curl
-											// 	$ch = curl_init();
-											// 	// Disable SSL verification
-											// 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-											// 	// Will return the response, if false it print the response
-											// 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-											// 	// Set the url
-											// 	curl_setopt($ch, CURLOPT_URL,$url);
-											// 	// Execute
-											// 	$result=json_decode(curl_exec($ch),true);
-											// 	// Closing
-											// 	curl_close($ch);      
-											// 	return $result;
-											// }
+											function getData($url)
+											{
+												//  Initiate curl
+												$ch = curl_init();
+												// Disable SSL verification
+												curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+												// Will return the response, if false it print the response
+												curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+												// Set the url
+												curl_setopt($ch, CURLOPT_URL,$url);
+												// Execute
+												$result=json_decode(curl_exec($ch),true);
+												// Closing
+												curl_close($ch);      
+												return $result;
+											}
 											   
-											// $link=array();	
-											// $links='';        	
-											// function getNextParser($url,$tmp)
-											// {
-											// 	$innerData = getData($url);
-											// 	foreach($innerData['data'] as $image)
-											// 		 {
-											// 		 $GLOBALS['links'].=$image['images'][0]['source']." ";
-											// 		 $tmp[]=($image['images'][0]['source']);
-											// 		 }
-											// 	if(isset($innerData['paging']['next'])){
-											// 		$tmp = getNextParser($innerData['paging']['next'],$tmp);
-											// 	}
-											// }
-											// // Main calling 
-											// $result = getData($url);     		
-											// foreach($result['albums']['data'] as $album)
-											// {
-											// 	$GLOBALS['links'].=$album['name']."||";
-											// 	foreach($album['photos']['data'] as $image)
-											// 	{
-											// 		 $GLOBALS['links'].= ($image['images'][0]['source']);
-											// 		$tmp[]=($image['images'][0]['source']);
-											// 		$cnt++;
-											// 	}
-											// 	if(isset($album['photos']['paging']['next']))
-											// 	{
-											// 		$tmp =(getNextParser($album['photos']['paging']['next'],$tmp));
-											// 	}	
-											// 	$GLOBALS['links'].=" , ";
-											// }												 
+											$link=array();	
+											$links='';        	
+											function getNextParser($url,$tmp)
+											{
+												$innerData = getData($url);
+												foreach($innerData['data'] as $image)
+													 {
+													 $GLOBALS['links'].=$image['images'][0]['source']." ";
+													 $tmp[]=($image['images'][0]['source']);
+													 }
+												if(isset($innerData['paging']['next'])){
+													$tmp = getNextParser($innerData['paging']['next'],$tmp);
+												}
+											}
+											// Main calling 
+											$result = getData($url);     		
+											foreach($result['albums']['data'] as $album)
+											{
+												$GLOBALS['links'].=$album['name']."||";
+												foreach($album['photos']['data'] as $image)
+												{
+													 $GLOBALS['links'].= ($image['images'][0]['source']);
+													$tmp[]=($image['images'][0]['source']);
+													$cnt++;
+												}
+												if(isset($album['photos']['paging']['next']))
+												{
+													$tmp =(getNextParser($album['photos']['paging']['next'],$tmp));
+												}	
+												$GLOBALS['links'].=" , ";
+											}												 
+											$_SESSION['links'] = $GLOBALS['links'];
+											$_SESSION['GraphNode'] = $graphNode;
 											// $zipfile = get_album($graphNode,$selected_album,$links);
 
 										
