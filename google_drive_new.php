9e5ad2cff5dff4350f6a1f964780667af695d807
abhishekgandhi11 <abhishekgandhi63@gmail.com>
2018-09-19 22:08:19 +0530

    n

--- google_drive_new.php
+++ google_drive_new.php
@@ -67,19 +67,17 @@ else
     
     #get links from albums
     function get_pictures($graphNode,$NameNLinks) {
-        echo "<pre>";
-            print_r($NameNLinks);
-        echo "</pre>";
-        // $album_pic_link = array();
-		// $graphnode = $graphNode;
-		// #user name and id for creation main user directory on server
-		// $user_name = $graphnode['name'];
-		// 	$index=0;
-		// 	$x=array();
-		// 	$urls = explode(' ', $NameNLinks[1]);
-		// 	foreach($urls as $url)
-		// 	   {
-        //         echo $url . "<br/>";
+         $album_pic_link = array();
+		$graphnode = $graphNode;
+		#user name and id for creation main user directory on server
+		$user_name = $graphnode['name'];
+			$index=0;
+			$x=array();
+			$urls = explode(' ', $NameNLinks[1]);
+            print_r($urls);
+            // foreach($urls as $url)
+			//    {
+            //     echo $url . "<br/>";
 		// 		// 	$download_file = file_get_contents($url);
 				// if($download_file==NULL){
 				// }
