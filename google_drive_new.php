1610ac796559ede7789138996444f9142387add9
abhishekgandhi11 <abhishekgandhi63@gmail.com>
2018-09-21 01:36:04 +0530

    dddd

--- google_drive_new.php
+++ google_drive_new.php
@@ -3,5 +3,23 @@ session_start();
 
 echo "hello";
 
+require_once 'google/vendor/autoload.php';
+
+ini_set('max_execution_time', 300);
+$client = new Google_Client();
+$client->setAuthConfigFile('api.json');
+$client->setRedirectUri('https://abhishekrtchallenge.herokuapp.com/google_drive_new.php');
+$client->addScope(Google_Service_Drive::DRIVE);
+
+if (! isset($_GET['code'])) 
+{
+    $auth_url = $client->createAuthUrl();
+    header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
+} 
+else 
+{
+    echo "else";
+}
+
 ?>
 
