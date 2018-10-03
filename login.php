ab9fb721db78516b9e35e11d0ef3c29f61663be1
RD11JUNE <rdesai526@gmail.com>
2018-10-04 00:19:58 +0530

    final

--- login.php
+++ login.php
@@ -7,19 +7,25 @@ session_start();
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Log-in</title>
+    <title></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php include("links.php"); ?>
 </head>
 <body >
-    <div class="loginback">
-    <div class="container">
-         <h1 align="center" style="color:black;margin-top:10px;font-size:40px;">Facebook Album Manager</h1> 
+<?php
+ include("header.php");
+header('location:login.php');
+?>
+    <div>
+    <div class="container" style="margin-top:50px;" align="centers">
+         <h1 style="color:white;margin-top:10px;font-size:40px;">Facebook Album Manager</h1> 
+         <hr>
+         <hr>
         <div class="row">
             <div class="col-md-4">
             </div>
             <div class="col-md-4">
-                <form name="f1">
+            <form name="f1">
                 <br><br>    
                 <br>
                 <br>
@@ -34,16 +40,14 @@ session_start();
                         // header('location:htmlspecialchars($loginUrl)')
                         echo "<a href='" . htmlspecialchars($loginUrl) . "'><h3 style='color: black;'>Log in with Facebook!</h3></a>";
                     ?>
-                </form>
+            </form>
             </div>
             <div class="col-md-4">
             </div>
             <br><br><br>
+            
         </div>
         <br><br><br>
-        <a href="#"><h3 style="color:white;margin-top:15px;font-size:20px;">Developed By: Abhishek Gandhi</h3></a>
-        <br>
-        <a href="#"><h3 style="color:white;margin-top:15px;font-size:20px;color:blue;">Email id: abhishekgandhi63@gmail.com</h3></a>
         </div>
     </div>
 </body>
